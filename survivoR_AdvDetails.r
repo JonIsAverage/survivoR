54bb7cb7db0906b9066df7c2ec5cf5d00068e769
JonIsAverage <150360113+JonIsAverage@users.noreply.github.com>
2023-12-08 15:37:48 -0600

    Updated AdvDetails with variable creations

--- survivoR_AdvDetails.r
+++ survivoR_AdvDetails.r
@@ -4,20 +4,28 @@ library(SparkR)
 
 # COMMAND ----------
 
-# MAGIC %sh
-# MAGIC wget -O /dbfs/advantage_details.rda https://raw.githubusercontent.com/doehm/survivoR/master/data/advantage_details.rda
+#assign R variables
+rdataname <- "advantage_details"
 
 # COMMAND ----------
 
-load("/dbfs/advantage_details.rda")
-your_data_frame_name <- "advantage_details"
+# MAGIC %sh
+# MAGIC advantage_details_variable="advantage_details"
+# MAGIC wget -O /dbfs/$advantage_details_variable.rda https://raw.githubusercontent.com/doehm/survivoR/master/data/$advantage_details_variable.rda
+
+# COMMAND ----------
+
+load(paste0("/dbfs/", rdataname,".rda"))
+your_data_frame_name <- rdataname
 df <- get(your_data_frame_name)
 sp_df <- as.DataFrame(df)
-createOrReplaceTempView(sp_df, "tmp_advantage_details")
+createOrReplaceTempView(sp_df, rdataname)
+createOrReplaceTempView(sp_df, (paste0("tmp_", rdataname)))
 
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC
 # MAGIC CREATE OR REPLACE TABLE bronze_adv_details
 # MAGIC AS
 # MAGIC SELECT *
@@ -26,5 +34,5 @@ createOrReplaceTempView(sp_df, "tmp_advantage_details")
 # MAGIC CREATE OR REPLACE TABLE gold_adv_details
 # MAGIC AS
 # MAGIC SELECT *
-# MAGIC FROM tmp_advantage_details
+# MAGIC FROM bronze_adv_details
 # MAGIC WHERE version = 'US';
